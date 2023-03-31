2365879fd28253f0ab267af5ff424877bac0fa82
MARVINAKI <kosakbig@gmial.com>
2023-03-31 22:51:14 +0500

    for check

--- src/main/Java/Application.java
+++ src/main/Java/Application.java
@@ -3,14 +3,28 @@ import DAO.EmployeeDAOImpl;
 import Model.City;
 import Model.Employee;
 
-import java.sql.SQLException;
-
 public class Application {
 
 	private static final EmployeeDAOImpl employeeDAO = new EmployeeDAOImpl();
 	private static final CityDAOImpl cityDAO = new CityDAOImpl();
 
-	public static void main(String[] args) throws SQLException {
-		System.out.println(cityDAO.updateByID(1, new City("UFA")));
+	public static void main(String[] args) {
+
+//		cityDAO.addCity(new City("TEST"));
+//		System.out.println(cityDAO.getAll());
+//		System.out.println(cityDAO.findByID(8));
+//		System.out.println(cityDAO.updateByID(8, new City("TESTOV")));
+//		System.out.println(cityDAO.findByID(8));
+//		System.out.println(cityDAO.deleteByID(8));
+//		System.out.println(cityDAO.getAll());
+//
+//		employeeDAO.addEmployee(new Employee("TEST", "TESTOV", 12));
+//		employeeDAO.addEmployee(new Employee("EXAMPLE", "EXAMPLOV", 34, 5));
+//		System.out.println(employeeDAO.getAll());
+//		System.out.println(employeeDAO.findById(30) + "\n" + employeeDAO.findById(31));
+//		System.out.println(employeeDAO.updateById(30, new Employee("EXAMPLE", "EXAMPLOV", 12, 1)));
+//		System.out.println(employeeDAO.findById(30));
+//		System.out.println(employeeDAO.deleteById(30));
+//		System.out.println(employeeDAO.deleteById(31));
 	}
 }
--- src/main/Java/DAO/EmployeeDAOImpl.java
+++ src/main/Java/DAO/EmployeeDAOImpl.java
@@ -4,14 +4,15 @@ import Config.ApplicationConnection;
 import Model.Employee;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class EmployeeDAOImpl implements EmployeeDAO {
 
 	private final ApplicationConnection appConnection = new ApplicationConnection();
-	private final CityDAOImpl cityDAO = new CityDAOImpl();
 
 	@Override
 	public boolean addEmployee(Employee employee) {
@@ -34,22 +35,75 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
 	@Override
 	public Employee findById(int id) {
-
-		return null;
+		Employee employee = null;
+		try (PreparedStatement preparedStatement = appConnection.getPreparedStatement("SELECT * FROM employee WHERE id = ?;")) {
+			preparedStatement.setInt(1, id);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			resultSet.next();
+			employee = new Employee(
+					resultSet.getString(2),
+					resultSet.getString(3),
+					resultSet.getInt(4),
+					resultSet.getInt(5));
+			employee.setId(resultSet.getInt(1));
+			return employee;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return employee;
+		}
 	}
 
 	@Override
 	public Map<Integer, Employee> getAll() {
-		return null;
+		Employee employee;
+		Map<Integer, Employee> employees = new LinkedHashMap<>();
+		try (PreparedStatement preparedStatement = appConnection.getPreparedStatement("SELECT * FROM employee;")) {
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				employee = new Employee(
+						resultSet.getString(2),
+						resultSet.getString(3),
+						resultSet.getInt(4),
+						resultSet.getInt(5));
+				employee.setId(resultSet.getInt("id"));
+				employees.put(resultSet.getInt("id"), employee);
+			}
+			return employees;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return employees;
+		}
 	}
 
 	@Override
 	public boolean updateById(int id, Employee employee) {
-		return false;
+		try (PreparedStatement preparedStatement = appConnection.getPreparedStatement("UPDATE employee SET name = ?, last_name = ?, age = ?, city_id = ? WHERE id = ?")) {
+			preparedStatement.setString(1, employee.getName());
+			preparedStatement.setString(2, employee.getLast_name());
+			preparedStatement.setInt(3, employee.getAge());
+			if (employee.getCity_id() == null || String.valueOf(employee.getCity_id()).trim().isEmpty()) {
+				preparedStatement.setNull(4, Types.INTEGER);
+			} else {
+				preparedStatement.setInt(4, employee.getCity_id()); // если не выходит за рамки city id!!!
+			}
+			preparedStatement.setInt(5, id);
+			preparedStatement.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	@Override
 	public boolean deleteById(int id) {
-		return false;
+		try (PreparedStatement preparedStatement = appConnection.getPreparedStatement("DELETE FROM employee WHERE id = ?;")) {
+			preparedStatement.setInt(1, id);
+			preparedStatement.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 }
